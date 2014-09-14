23f19f6ddfd50f6a284c8b6725d7607786d4ebe6
Tushar <tushar@Macintosh-2.local>
2014-09-14 19:35:03 +0530

    add to cart and check out

--- js/controllers.js
+++ js/controllers.js
@@ -1,43 +1,42 @@
 angular.module('starter.controllers', ['myservices'])
 
 .controller('DashCtrl', function ($scope, $stateParams, MyServices) {
-    
-    
+
+
 })
 
 .controller('CategoryCtrl', function ($scope, $stateParams, MyServices) {
-    
+
 })
 
 .controller('ItemCtrl', function ($scope, $stateParams, MyServices) {
-    var categoryId= $stateParams.cid;
+    var categoryId = $stateParams.cid;
 
-    var onsuccess = function(data, status)
-    {
+    var onsuccess = function (data, status) {
         $scope.products = data;
-        
+
         $scope.productItem = [];
         var change = 11;
         var counter = 0;
-        $scope.loadMore = function() {
+        $scope.loadMore = function () {
             console.log(":");
-            if(counter < $scope.products.product.length) {
-            for(var i=counter;i<=(counter+change);i++){
-                $scope.productItem.push($scope.products.product[i]);
-            };
+            if (counter < $scope.products.product.length) {
+                for (var i = counter; i <= (counter + change); i++) {
+                    $scope.productItem.push($scope.products.product[i]);
+                };
                 counter += change + 1;
             };
             $scope.$broadcast('scroll.infiniteScrollComplete');
         };
 
         $scope.loadMore();
-       
+
     };
     MyServices.getproductbycategory(categoryId).success(onsuccess);
-    
+
 })
 
-.controller('ProductCtrl', function ($scope, $stateParams,$ionicSlideBoxDelegate ,$timeout ,$ionicLoading, MyServices) {
+.controller('ProductCtrl', function ($scope, $stateParams, $ionicSlideBoxDelegate, $timeout, $ionicLoading, MyServices) {
     $ionicLoading.show({
         template: 'Loading...',
         animation: 'fade-in',
@@ -45,44 +44,89 @@ angular.module('starter.controllers', ['myservices'])
         maxWidth: 200,
         showDelay: 500
     });
-    var productId= $stateParams.id;
+    var productId = $stateParams.id;
 
-    var onsuccess = function(data, status)
-    {
+    var onsuccess = function (data, status) {
         $scope.item = data;
+        $scope.item.product.quantity2 = 1;
         console.log(data);
     };
     MyServices.getproductdetails(productId).success(onsuccess);
-    
+
+    //Add to cart
+    $scope.addtocart = function (id, name, price, quantity) {
+
+        MyServices.addtocart(id, name, price, quantity);
+
+    };
+
     //SLIDE BOX
-    $scope.nextSlide = function() {
+    $scope.nextSlide = function () {
         $ionicSlideBoxDelegate.next();
     };
-    $scope.prevSlide = function() {
+    $scope.prevSlide = function () {
         $ionicSlideBoxDelegate.previous();
     };
-    $timeout(function(){
+    $timeout(function () {
         $ionicSlideBoxDelegate.update();
         $ionicLoading.hide();
-    },2000);
+    }, 2000);
 })
 
-.controller('LookbookCtrl', function ($scope, $stateParams) {
-})
+.controller('LookbookCtrl', function ($scope, $stateParams) {})
 
 .controller('LookbookitemCtrl', function ($scope) {})
 
 .controller('AccountCtrl', function ($scope) {})
 
-.controller('CartCtrl', function ($scope) {})
+.controller('CartCtrl', function ($scope, $stateParams, MyServices) {
+    //Product details
+    var onproductsuccess = function (data, status) {
+        for (var i = 0; i < $scope.products.length; i++) {
+            if ($scope.products[i].id == data.product.id) {
+                $scope.products[i].image = data.productimage[0].image;
+            }
+        }
+    };
+
+    var onsuccess = function (data, status) {
+        $scope.products = data;
+
+        for (var i = 0; i < data.length; i++) {
+            MyServices.getproductdetails(data[i].id).success(onproductsuccess);
+        }
+
+    };
+    MyServices.getcart().success(onsuccess);
+
+    //Remove item
+    var ondeletesuccess = function () {
+        MyServices.getcart().success(onsuccess);
+    };
+    $scope.deletecart = function (id) {
+        MyServices.deletecartfromsession(id).success(ondeletesuccess);
+    };
+    
+    //Total
+    var ontotalsuccess = function (data, status) {
+        $scope.gettotal = data;
+    };
+    MyServices.totalcart().success(ontotalsuccess);
+
+})
 
 .controller('ContactCtrl', function ($scope) {})
 
-.controller('CheckoutCtrl', function ($scope) {
+.controller('CheckoutCtrl', function ($scope, MyServices) {
+    var ontotalsuccess = function (data, status) {
+        $scope.gettotal = data;
+    };
+    MyServices.totalcart().success(ontotalsuccess);
+    
     $scope.$on('$viewContentLoaded', function () {
         var handler = StripeCheckout.configure({
-            key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
-            image: '/square-image.png',
+            key: 'pk_live_LummdQUKjom4PnlfHJhLPDKC',
+            image: 'img/logo.png',
             token: function (token) {
                 // Use the token to create the charge with a server-side script.
                 // You can access the token ID with `token.id`
@@ -92,9 +136,10 @@ angular.module('starter.controllers', ['myservices'])
         document.getElementById('customButton').addEventListener('click', function (e) {
             // Open Checkout with further options
             handler.open({
-                name: 'Demo Site',
-                description: '2 widgets ($20.00)',
-                amount: 2000
+                name: 'Lyla Loves',
+                description: 'Total: £'+$scope.gettotal+'.00',
+                currency: 'GBP',
+                amount: ($scope.gettotal*100)
             });
             e.preventDefault();
         });
@@ -102,11 +147,10 @@ angular.module('starter.controllers', ['myservices'])
 })
 
 .controller('LoginCtrl', function ($scope) {
-        
-$scope.loginfunction = function()
-{
-        
-};
+
+    $scope.loginfunction = function () {
+
+    };
 
 })
 
--- js/services.js
+++ js/services.js
@@ -3,8 +3,8 @@ var adminurl = 'http://zibacollection.co.uk/lyla/index.php/json/';
 var myservices = angular.module('myservices', [])
 
 .factory('MyServices', function ($http, $location) {
-    var retailer=0;
-    var category=0;
+    var retailer = 0;
+    var category = 0;
     return {
         getproductdetails: function (product, category) {
             return $http.get(adminurl + 'getproductdetails', {
@@ -24,5 +24,68 @@ var myservices = angular.module('myservices', [])
                 withCredentials: true
             });
         },
+        addtocart: function (id, name, price, quantity) {
+            return $http.post(adminurl + 'addtocart?product=' + id + '&productname=' + name + "&quantity=" + quantity + "&price=" + price, {}, {
+                withCredentials: true
+            });
+        },
+        getcart: function () {
+            return $http.post(adminurl + 'showcart', {}, {
+                withCredentials: true
+            });
+            //return cart;
+        },
+        gettotalcart: function () {
+            return $http.post(adminurl + 'totalitemcart', {}, {
+                withCredentials: true
+            });
+            //return cart;
+        },
+        totalcart: function () {
+            return $http.post(adminurl + 'totalcart', {}, {
+                withCredentials: true
+            });
+            //return cart;
+        },
+        deletecart: function (id) {
+
+            subtotal = this.calcsubtotal();
+            return subtotal;
+        },
+        deletecartfromsession: function (id) {
+            return $http.post(adminurl + 'deletecart?id=' + id, {}, {
+                withCredentials: true
+            });
+        },
+        savecart: function (uid, id, quantity) {
+            console.log(cart);
+            for (var i = 0; i < cart.length; i++) {
+                if (cart[i].id == id) {
+                    cart[i].quantity = quantity;
+                    console.log(cart[i].name);
+                    returntwo.state = $http.post(adminurl + 'addtocart?user=' + uid + '&product=' + id + "&quantity=" + cart[i].quantity, {}, {
+                        withCredentials: true
+                    });
+                }
+
+            }
+            console.log(cart);
+            returntwo.subtotal = this.calcsubtotal();
+            return returntwo;
+        },
+        calcsubtotal: function () {
+            subtotal = 0;
+            for (var i = 0; i < cart.length; i++) {
+                subtotal += cart[i].price * cart[i].quantity;
+            }
+            console.log(subtotal);
+            return subtotal;
+
+        },
+        gettotalproductsincart: function (data, status) {
+            console.log(data);
+            TemplateService.totalproducts = data;
+            return 0;
+        }
     }
 });
